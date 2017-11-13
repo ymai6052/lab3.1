87492bec4a405f21677cb2b5f94dd63eb8117e6c
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-13 13:15:54 -0500

    product array

--- Lab3.1/src/lab/functions.java
+++ Lab3.1/src/lab/functions.java
@@ -4,7 +4,10 @@ public class functions {
 	public static void main (String [] args)
 	{
 		int[] nums= {7,2,3,4,5,6,1,7,7,8};
-		printArray(removeDuplicates(nums));
+		int [] nums2 = {3,7,8,2,6,7,8,9,2,1};
+		//printArray(removeDuplicates(nums));
+		print2dArray(productArray(nums, nums2));
+
 	}
 	public static int[] removeDuplicates(int [] list)
 	{
@@ -12,12 +15,11 @@ public class functions {
 		int end=list.length;
 		for (int i = 0; i < end;i++)
 		{
-			for (int j = 0; j < end; j++)
+			for (int j = i+1; j < end; j++)
 			{
 				if(list[i] == list[j])
 				{
 					int shift = j;
-					System.out.println(j);
 					for (int x = j+1; x < end; x++, shift++)
 					{
 						list[shift] = list[x];
@@ -43,9 +45,32 @@ public class functions {
 		}
 		System.out.println();
 	}
-	
+	public static void print2dArray(int [][] Arr )
+	{
+		
+		 for (int i = 0 ; i < Arr.length; i++)
+		 {
+			 for (int j = 0; j < Arr.length; j++)
+			 {
+				 System.out.print(Arr[j][i]+" ");
+			 }
+			 System.out.println();
+		 }
+		 System.out.println();
+	}
 	public static int[][] productArray(int[] arr1, int[] arr2)
 	{
+		int [][] newarray = new int[arr1.length][arr2.length
+		                                         ];
+		for(int i=0;i<arr1.length;i++)
+		{
+			for(int j=i;j<arr2.length;j++)
+			{
+				newarray[i][0] = arr1[i]*arr2[i];
+				newarray[0][j] = arr2[j]*arr2[j];
+			}
+		}
+		return newarray;
 		
 	}
 	public static int[ ][ ] pascalTriangle(int n)
