82c42eedbba96e97c5626981e10d04a288685e17
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-14 13:16:15 -0500

    pascal triangle

--- Lab3.1/src/lab/functions.java
+++ Lab3.1/src/lab/functions.java
@@ -4,12 +4,12 @@ public class functions {
 	public static void main (String [] args)
 	{
 		int[] nums= {7,2,3,4,5,6,1,7,7,8};
-		int [] nums2 = {3,7,8,2,6,7,8,9,2,1};
-		int[] test= {2,2,3};
-		int[] test1= {2,3,4};
+		int[] nums2 = {4,7,8,2,6,7,8,9,2,1};
 		//printArray(removeDuplicates(nums));
-		print2dArray(productArray(test, test1));
-
+		//print2dArray(productArray(nums, nums2));
+		//print2dArray(pascalTriangle(5));
+		printPascalTriangle(pascalTriangle(5));
+		
 	}
 	public static int[] removeDuplicates(int [] list)
 	{
@@ -39,6 +39,7 @@ public class functions {
 		}
 		return newlist;
 	}
+	
 	public static void printArray(int[] Arr)
 	{
 		for(int i=0;i<Arr.length;i++)
@@ -49,18 +50,12 @@ public class functions {
 	}
 	public static int[][] productArray(int[] arr1, int[] arr2)
 	{
-		int [][] newarray = new int[arr1.length][arr2.length
-		                                         ];
+		int [][] newarray = new int[arr1.length][arr2.length];
 		for(int i=0;i<arr1.length;i++)
 		{
-			int counter=0;
-			System.out.println(i);
 			for(int j=0;j<arr2.length;j++)
 			{
-				newarray[i][counter]=arr1[i]*arr2[j];
-				counter++;
-				System.out.println();
-				System.out.println(counter);
+				newarray[i][j]=arr1[i]*arr2[j];
 			}
 		}
 		return newarray;
@@ -80,10 +75,38 @@ public class functions {
 	}
 	public static int[ ][ ] pascalTriangle(int n)
 	{
-		
+		int [][] newarray = new int[n][];
+		for (int i = 0; i < n; i++)
+		{
+			newarray[i] = new int[i+1];
+			for(int j=0;j<i+1;j++)
+			{
+				if(i==0||j<1)
+					newarray[i][j]=1;
+				else if(i>2)
+				{
+					System.out.print(i);
+					newarray[i][j]=newarray[i-1][j-1]+newarray[i-1][j];
+					
+				}
+					newarray[i][j]=1;
+					//else
+					//newarray[i][j]=newarray[i-1][j-1]+newarray[i-1][j];
+					
+			}
+		}
+		return newarray;
 	}
 	public static void printPascalTriangle(int[ ] [ ] triangle)
 	{
-		
+		for (int i = 0 ; i < triangle.length; i++)
+		 {
+			 for (int j = 0; j < triangle[i].length; j++)
+			 {
+				 System.out.print(triangle[i][j]+" ");
+			 }
+			 System.out.println();
+		 }
+		 System.out.println();
 	}
 }
